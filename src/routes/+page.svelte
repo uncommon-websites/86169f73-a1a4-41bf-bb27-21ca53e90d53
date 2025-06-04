97fa4b9827299d53edc116aa1c9b1d29876975a5
Uncle Common <krishnerkar@gmail.com>
2025-06-04 23:51:21 +0000

    Initial hero generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,15 @@
 	imageSrc="/sample.png"
 	generating
 />
-<LogoScroller label="" generating />
+<LogoScroller
+	label="Trusted by industry leaders"
+	logoUrls={[
+		'https://logo.clearbit.com/stripe.com',
+		'https://logo.clearbit.com/asana.com',
+		'https://logo.clearbit.com/datadog.com',
+		'https://logo.clearbit.com/zendesk.com'
+	]}
+/>
 
 <Summary
 	generating
